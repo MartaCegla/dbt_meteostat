d42e51649114a2b1b2a7823b4e63cfa49abb383b
Marta Chmielewska - Cegła <MartaChmielewska-Cegla@MacBook-Pro-5.local>
2024-11-11 14:15:55 +0100

    added hourly

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -1,27 +1,27 @@
 WITH daily_data AS (
-        SELECT * 
-        FROM {{ref('staging_weather_daily')}}
-    ),
-    add_features AS (
-        SELECT *
-    		, DATE_PART('day', date) AS date_day
-    		, DATE_PART('month', date) AS date_month
-    		, DATE_PART('year', date) AS date_year
-    		, EXTRACT(WEEK from date) AS cw -- Adjusted for general SQL compatibility
-    		, TO_CHAR(date, 'Month') AS month_name
-    		, TO_CHAR(date, 'Day') AS weekday
-        FROM daily_data 
-    ),
-    add_more_features AS (
-        SELECT *
-    		, (CASE 
-    			WHEN date_part('month', date) in ('December', 'January', 'February') THEN 'winter'
-    			WHEN date_part('month', date) in ('March', 'April', 'May') THEN 'spring'
-                WHEN date_part('month', date) in ('June', 'July', 'August') THEN 'summer'
-                WHEN date_part('month', date) in ('September', 'October', 'November') THEN 'autumn'
-    		END) AS season
-        FROM add_features
-    )
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
     SELECT *
-    FROM add_more_features
-    ORDER BY date
+		, DATE_PART('day', date) AS date_day
+		, DATE_PART('month', date) AS date_month
+		, DATE_PART('year',date) AS date_year
+		, EXTRACT (week from date) AS cw
+		, to_char (date,'FMMonth') AS month_name
+		, to_char (date, 'day') AS weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+		, (CASE 
+			WHEN date_month in (12, 1, 2) THEN 'winter'
+			WHEN date_month in (3, 4, 5) THEN 'spring'
+            WHEN date_month in (6, 7, 8)  THEN 'summer'
+            WHEN date_month in (9, 10, 11) THEN 'autumn'
+		END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
