0356d2a64b203c6f2c6b405f03c2b6c67a3a4b0b
Marta Chmielewska - Cegła <MartaChmielewska-Cegla@MacBook-Pro-5.local>
2024-11-11 14:39:59 +0100

    new folder

--- models/prep/prep_flights.sql
+++ models/prep/prep_flights.sql
@@ -5,23 +5,23 @@ WITH flights_one_month AS (
     flights_cleaned AS(
         SELECT flight_date::DATE
                 ,TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time
-                ,... AS sched_dep_time
+                ,TO_CHAR(sched_dep_time_, 'fm0000')::TIME AS sched_dep_time
                 ,dep_delay
     		    ,(dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
-                ,...::TIME AS arr_time
-                ,...::TIME AS sched_arr_time
+                ,TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time
+                ,to_char(sched_arr_time, 'fm0000')::TIME AS sched_arr_time
                 ,arr_delay
-                ,(...) AS arr_delay_interval
+                ,(arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval
                 ,airline
                 ,tail_number
                 ,flight_number
                 ,origin
                 ,dest
                 ,air_time
-                ,(...) AS air_time_interval
+                ,(air_time * '1 minute'::INTERVAL) AS air_time_interval
                 ,actual_elapsed_time
-                ,(...) AS actual_elapsed_time_interval
-                ,(distance * 0.621371)::NUMERIC(6,2) AS distance_km -- see instruction hint
+                ,(actual_elapsed_time* '1 minute'::INTERVAL) AS actual_elapsed_time_interval
+                ,(distance / 0.621371)::NUMERIC(6,2) AS distance_km -- see instruction hint
                 ,cancelled
                 ,diverted
         FROM flights_one_month
