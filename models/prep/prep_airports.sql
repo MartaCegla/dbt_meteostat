3eb19da647c61697f414c3db50895c2d1437af64
Marta Chmielewska - Cegła <MartaChmielewska-Cegla@MacBook-Pro-5.local>
2024-11-11 14:29:55 +0100

    comas

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,16 +1,16 @@
 WITH airports_reorder AS (
     SELECT 
         faa
-        name,
-        city,
-        latt,
-        lon,
-        alt,
-        tz,
-        dst,
-        city,
-        country,
-        region
+        ,name
+        ,city
+        ,latt
+        ,lon
+        ,alt
+        ,tz
+        ,dst
+        ,city
+        ,country
+        ,region
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
